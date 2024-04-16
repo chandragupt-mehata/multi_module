8b99f0de64c419bca85afce0458cb2ff29ebc78b
chandragupt <chandraguptmehata@gmail.com>
2024-04-16 17:30:46 +0530

    initial changes

---
+++ moduleb/src/main/java/com/example/moduleb/AbiClass.kt
@@ -0,0 +1,8 @@
+package com.example.moduleb
+
+class AbiClass {
+
+    fun abiTestFunction() {
+        println("I am inside of abiTestFunction")
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
+rootProject.name = "My Multi Module Application"
+include(":app")
+include(":modulea")
+include(":moduleb")
